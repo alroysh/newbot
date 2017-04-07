abe80245b3079050415df33b35bf39cd449f6cd4
alroysh <cicak.jatuh02@gmail.com>
2017-04-08 00:51:59 +0800

    Update bot.php

--- bot.php
+++ bot.php
@@ -1,6 +1,8 @@
 <?php
 	date_default_timezone_set('Asia/Singapore');
 	$wita= date('H.i.s');
+	date_default_timezone_set('Asia/Jakarta');
+	$wib= date('H.i.s');
 	require_once('./line_class.php');
 	$channelAccessToken = 'bxcs67UsXL4dg6qSR4Nojg1djzE2QaP3RvedqZ2nY/2b+U6ypsmuoDr4j74SqKBQS2S8nFXRiOyfieRMLU2CEcqz570pODeTjUk8H4Y+AyhcO5qiEEj95HWSwIk23KR1AEIQjnUOw/JG+vdvTeVn1AdB04t89/1O/w1cDnyilFU=';
 	$channelSecret = '2ad35467614230c7a6dfe8e158e95988';
@@ -170,7 +172,7 @@ if($message['type']=='text')
 			'messages' => array(
 			              array(
 					  'type' => 'text',					
-					   'text' => 'Now ' . $wita
+					   'text' => 'Now ' . $wib
 										)
 								)
 							);
