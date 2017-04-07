72e8fe91f1be639214c0ac5fa001c008f91d4f11
alroysh <cicak.jatuh02@gmail.com>
2017-04-07 14:01:02 +0800

    Update bot.php

--- bot.php
+++ bot.php
@@ -1,228 +1,121 @@
-	<?php
-	date_default_timezone_set('Asia/Singapore');
-	require_once('./line_class.php');
-	$channelAccessToken = 'bxcs67UsXL4dg6qSR4Nojg1djzE2QaP3RvedqZ2nY/2b+U6ypsmuoDr4j74SqKBQS2S8nFXRiOyfieRMLU2CEcqz570pODeTjUk8H4Y+AyhcO5qiEEj95HWSwIk23KR1AEIQjnUOw/JG+vdvTeVn1AdB04t89/1O/w1cDnyilFU='; //sesuaikan 
-	$channelSecret = '2ad35467614230c7a6dfe8e158e95988';//sesuaikan
-	$client = new LINEBotTiny($channelAccessToken, $channelSecret);
-	//var_dump($client->parseEvents());
-	//$_SESSION['userId']=$client->parseEvents()[0]['source']['userId'];
-	/*
-	{
-	  "replyToken": "nHuyWiB7yP5Zw52FIkcQobQuGDXCTA",
-	  "type": "message",
-	  "timestamp": 1462629479859,
-	  "source": {
-		"type": "user",
-		"userId": "U206d25c2ea6bd87c17655609a1c37cb8"
-	  },
-	  "message": {
-		"id": "325708",
-		"type": "text",
-		"text": "Hello, world"
-	  }
-	}
-	*/
-	$userId 	= $client->parseEvents()[0]['source']['userId'];
-	$replyToken = $client->parseEvents()[0]['replyToken'];
-	$timestamp	= $client->parseEvents()[0]['timestamp'];
-	$message 	= $client->parseEvents()[0]['message'];
-	$messageid 	= $client->parseEvents()[0]['message']['id'];
-	$profil = $client->profil($userId);
-	$pesan_datang = $message['text'];
-	$wita= date_default_timezone_set['Asia/Singapore'];
-	$jam = date("H.i.s ");
-	//pesan bergambar
-	if($message['type']=='text')
-	{
-		if($pesan_datang=='Halo')
-		{
-			
-			
-			$balas = array(
-								'replyToken' => $replyToken,														
-								'messages' => array(
-									array(
-											'type' => 'text',					
-											'text' => 'Halo ' .$profil->displayName.''
-										)
-								)
-							);
-					
-		}
-		else
-				if($pesan_datang=='link fotoku')
-		{
-			
-			
-			$balas = array(
-								'replyToken' => $replyToken,														
-								'messages' => array(
-									array(
-											'type' => 'text',					
-											'text' => 'Link Foto Kamu : ' .$profil->pictureUrl.''
-										)
-								)
-							);
-					
-		}
-		else
-				if($pesan_datang=='status')
-		{
-			
-			
-			$balas = array(
-								'replyToken' => $replyToken,														
-								'messages' => array(
-									array(
-											'type' => 'text',					
-											'text' => 'Status Message Kamu : ' .$profil->statusMessage.''
-										)
-								)
-							);
-					
-		}
-		else
-				if($pesan_datang=='stickernya mana?')
-		{
-			
-			
-			$balas = array(
-								'replyToken' => $replyToken,														
-								'messages' => array(
-									array(
-											'type' => 'sticker',					
-											'packageId'=> '1',
-											'stickerId'=> '2'
-										)
-								)
-							);
-					
-		}
-		else
-		if($pesan_datang=='2')
-		{
-			$get_sub = array();
-			$aa =   array(
-							'type' => 'image',									
-							'originalContentUrl' => 'https://medantechno.com/line/images/bolt/1000.jpg',
-							'previewImageUrl' => 'https://medantechno.com/line/images/bolt/240.jpg'	
-							
-						);
-			array_push($get_sub,$aa);	
-			$get_sub[] = array(
-										'type' => 'text',									
-										'text' => 'Halo '.$profil->displayName.', Anda memilih menu 2, harusnya gambar muncul.'
-									);
-			
-			$balas = array(
-						'replyToken' 	=> $replyToken,														
-						'messages' 		=> $get_sub
-					 );	
-			/*
-			$alt = array(
-								'replyToken' => $replyToken,														
-								'messages' => array(
-									array(
-											'type' => 'text',					
-											'text' => '[*] '.join(str(f) for f in dataResult).'
-										)
-								)
-							);
-			*/
-			//$client->replyMessage($alt);
-		}
-		else
-		if($pesan_datang=='3')
-		{
-			
-			$balas = array(
-								'replyToken' => $replyToken,														
-								'messages' => array(
-									array(
-											'type' => 'text',					
-											'text' => 'Fungsi PHP base64_encode medantechno.com :'. base64_encode("medantechno.com")
-										)
-								)
-							);
-					
-		}
-		else
-		if($pesan_datang=='/jam')
-		{
-			
-			$balas = array(
-								'replyToken' => $replyToken,														
-								'messages' => array(
-									array(
-											'type' => 'text',					
-											'text' => 'Wita : '. date('H.i.s')
-										)
-								)
-							);
-					
-		}
-		else
-		if($pesan_datang=='Lokasi Bot')
-		{
-			
-			$balas = array(
-								'replyToken' => $replyToken,														
-								'messages' => array(
-									array(
-											'type' => 'location',					
-											'title' => 'Lokasi Saya.. Klik Detail',					
-											'address' => 'McDonald',					
-											'latitude' => '-8.700088',					
-											'longitude' => '115.178097' 
-										)
-								)
-							);
-					
-		}
-		else
-		if($pesan_datang=='7')
-		{
-			
-			$balas = array(
-								'replyToken' => $replyToken,														
-								'messages' => array(
-									array(
-											'type' => 'text',					
-											'text' => 'Testing PUSH pesan ke anda'
-										)
-								)
-							);
-							
-			$push = array(
-								'to' => $userId,									
-								'messages' => array(
-									array(
-											'type' => 'text',					
-											'text' => 'Pesan ini dari medantechno.com'
-										)
-								)
-							);
-							
-							
-		}
-	}else if($message['type']=='sticker')
-	{	
-		$balas = array(
-								'replyToken' => $replyToken,														
-								'messages' => array(
-									array(
-											'type' => 'text',									
-											'text' => 'Terimakasih stikernya... '										
-										
-										)
-								)
-							);
-		
-$response = $bot->leaveRoom('<groupId>');
-echo $response->getHTTPStatus() . ' ' . $response->getRawBody();		
-	}
-	 
-	$result =  json_encode($balas);
-	//$result = ob_get_clean();
-	file_put_contents('./balasan.json',$result);
-	$client->replyMessage($balas);
+<?php
+
+/**
+ * Copyright 2016 LINE Corporation
+ *
+ * LINE Corporation licenses this file to you under the Apache License,
+ * version 2.0 (the "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at:
+ *
+ *   https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+
+/*
+ * This polyfill of hash_equals() is a modified edition of https://github.com/indigophp/hash-compat/tree/43a19f42093a0cd2d11874dff9d891027fc42214
+ *
+ * Copyright (c) 2015 Indigo Development Team
+ * Released under the MIT license
+ * https://github.com/indigophp/hash-compat/blob/43a19f42093a0cd2d11874dff9d891027fc42214/LICENSE
+ */
+if (!function_exists('hash_equals')) {
+    defined('USE_MB_STRING') or define('USE_MB_STRING', function_exists('mb_strlen'));
+
+    function hash_equals($knownString, $userString)
+    {
+        $strlen = function ($string) {
+            if (USE_MB_STRING) {
+                return mb_strlen($string, '8bit');
+            }
+
+            return strlen($string);
+        };
+
+        // Compare string lengths
+        if (($length = $strlen($knownString)) !== $strlen($userString)) {
+            return false;
+        }
+
+        $diff = 0;
+
+        // Calculate differences
+        for ($i = 0; $i < $length; $i++) {
+            $diff |= ord($knownString[$i]) ^ ord($userString[$i]);
+        }
+        return $diff === 0;
+    }
+}
+
+class LINEBotTiny
+{
+    public function __construct($channelAccessToken, $channelSecret)
+    {
+        $this->channelAccessToken = $channelAccessToken;
+        $this->channelSecret = $channelSecret;
+    }
+
+    public function parseEvents()
+    {
+        if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
+            http_response_code(405);
+            error_log("Method not allowed");
+            exit();
+        }
+
+        $entityBody = file_get_contents('php://input');
+
+        if (strlen($entityBody) === 0) {
+            http_response_code(400);
+            error_log("Missing request body");
+            exit();
+        }
+
+        if (!hash_equals($this->sign($entityBody), $_SERVER['HTTP_X_LINE_SIGNATURE'])) {
+            http_response_code(400);
+            error_log("Invalid signature value");
+            exit();
+        }
+
+        $data = json_decode($entityBody, true);
+        if (!isset($data['events'])) {
+            http_response_code(400);
+            error_log("Invalid request body: missing events property");
+            exit();
+        }
+        return $data['events'];
+    }
+
+    public function replyMessage($message)
+    {
+        $header = array(
+            "Content-Type: application/json",
+            'Authorization: Bearer ' . $this->channelAccessToken,
+        );
+
+        $context = stream_context_create(array(
+            "http" => array(
+                "method" => "POST",
+                "header" => implode("\r\n", $header),
+                "content" => json_encode($message),
+            ),
+        ));
+
+        $response = file_get_contents('https://api.line.me/v2/bot/message/reply', false, $context);
+        if (strpos($http_response_header[0], '200') === false) {
+            http_response_code(500);
+            error_log("Request failed: " . $response);
+        }
+    }
+
+    private function sign($body)
+    {
+        $hash = hash_hmac('sha256', $body, $this->channelSecret, true);
+        $signature = base64_encode($hash);
+        return $signature;
+    }
+}
