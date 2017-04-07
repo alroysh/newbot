e9b8b21f7d5a6d9c64aed65e71118586366df309
alroysh <cicak.jatuh02@gmail.com>
2017-04-07 23:57:15 +0800

    Update bot.php

--- bot.php
+++ bot.php
@@ -170,7 +170,7 @@ if($message['type']=='text')
 			'messages' => array(
 			              array(
 					  'type' => 'text',					
-					   'text' => 'Now '. date('H.i.s')
+					   'text' => 'Now '. date('H.i.s,time()+60*60*7')
 										)
 								)
 							);
