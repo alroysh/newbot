4e4ace3be0b52ad7c3f56d43358844a2606c530c
alroysh <cicak.jatuh02@gmail.com>
2017-04-08 01:37:19 +0800

    Update bot.php

--- bot.php
+++ bot.php
@@ -216,7 +216,7 @@ if($message['type']=='text')
 			'messages' => array(
 			              array(
 					  'type' => 'text',					
-					   'text' => 'Hasilnya '. $a 
+					   'text' => 'Hasilnya '. $a + . $b
 										)
 								)
 							);
