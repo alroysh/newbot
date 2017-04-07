74819ade5fde348cf9b29ef9403e35de84850328
alroysh <cicak.jatuh02@gmail.com>
2017-04-07 18:49:43 +0800

    Update and rename echo_bot.php to bot.php

--- bot.php
+++ bot.php
@@ -1,13 +1,27 @@
 <?php
-
-require_once('./LINEBotTiny.php');
-
-$channelAccessToken = 'bxcs67UsXL4dg6qSR4Nojg1djzE2QaP3RvedqZ2nY/2b+U6ypsmuoDr4j74SqKBQS2S8nFXRiOyfieRMLU2CEcqz570pODeTjUk8H4Y+AyhcO5qiEEj95HWSwIk23KR1AEIQjnUOw/JG+vdvTeVn1AdB04t89/1O/w1cDnyilFU=';
-$channelSecret = '2ad35467614230c7a6dfe8e158e95988';
-$client = new LINEBotTiny($channelAccessToken, $channelSecret);
-
-
-
+	date_default_timezone_set('Asia/Singapore');
+	require_once('./line_class.php');
+	$channelAccessToken = 'bxcs67UsXL4dg6qSR4Nojg1djzE2QaP3RvedqZ2nY/2b+U6ypsmuoDr4j74SqKBQS2S8nFXRiOyfieRMLU2CEcqz570pODeTjUk8H4Y+AyhcO5qiEEj95HWSwIk23KR1AEIQjnUOw/JG+vdvTeVn1AdB04t89/1O/w1cDnyilFU=';
+	$channelSecret = '2ad35467614230c7a6dfe8e158e95988';
+	$client = new LINEBotTiny($channelAccessToken, $channelSecret);
+	//var_dump($client->parseEvents());
+	//$_SESSION['userId']=$client->parseEvents()[0]['source']['userId'];
+	/*
+	{
+	  "replyToken": "nHuyWiB7yP5Zw52FIkcQobQuGDXCTA",
+	  "type": "message",
+	  "timestamp": 1462629479859,
+	  "source": {
+		"type": "user",
+		"userId": "U206d25c2ea6bd87c17655609a1c37cb8"
+	  },
+	  "message": {
+		"id": "325708",
+		"type": "text",
+		"text": "Hello, world"
+	  }
+	}
+	*/
 	$userId 	= $client->parseEvents()[0]['source']['userId'];
 	$replyToken = $client->parseEvents()[0]['replyToken'];
 	$timestamp	= $client->parseEvents()[0]['timestamp'];
@@ -212,4 +226,3 @@ echo $response->getHTTPStatus() . ' ' . $response->getRawBody();
 	//$result = ob_get_clean();
 	file_put_contents('./balasan.json',$result);
 	$client->replyMessage($balas);
-
