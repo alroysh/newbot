957f5e0124a0568fa644d15d9dcf01759127e6c1
alroysh <cicak.jatuh02@gmail.com>
2017-04-08 00:14:13 +0800

    Update bot.php

--- bot.php
+++ bot.php
@@ -170,7 +170,7 @@ if($message['type']=='text')
 			'messages' => array(
 			              array(
 					  'type' => 'text',					
-					   'text' => 'Now '. date('H.i.s',$wib)
+					   'text' => 'Now '. date($wib,'H.i.s')
 										)
 								)
 							);
