9df080fe23febf3fe50871c58220f0e0f303ef23
alroysh <cicak.jatuh02@gmail.com>
2017-04-08 13:13:40 +0800

    Update bot.php

--- bot.php
+++ bot.php
@@ -83,7 +83,7 @@ if($message['type']=='text')
 			'messages' => array(
 					array(
 					'type' => 'text',					
-					'text' => 'IP : ' . $ip. ' Browser : ' .$browser. 
+					'text' => 'IP : ' . $ip. ' Browser : ' .$browser
 										)
 								)
 							);
