14232aeab7decaacf03f80237c4d8deced7e66f1
alroysh <cicak.jatuh02@gmail.com>
2017-04-09 21:12:59 +0800

    Update bot.php

--- bot.php
+++ bot.php
@@ -307,11 +307,6 @@ if($message['type']=='text')
 						'type' => 'message',
 						    'label' => 'Keyword',
 						    'text' => 'keyword'
-						],
-							[
-						'type' => 'message',
-						    'label' => 'Jam',
-						    'text' => '/jam'
 						]
 						]
 						
